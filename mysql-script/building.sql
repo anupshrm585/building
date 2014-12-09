6bc3e397f0d0a74b31a76fc69da733dd47a98927
Anup Sharma <anupshrm585@gmail.com>
2014-12-09 18:31:04 +0530

    Mysql - 09-12-2014 script

--- mysql-script/building.sql
+++ mysql-script/building.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.1.33, for Win32 (ia32)
+-- MySQL dump 10.13  Distrib 5.5.36, for Win32 (x86)
 --
 -- Host: localhost    Database: building
 -- ------------------------------------------------------
--- Server version	5.1.33-community
+-- Server version	5.5.36-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -23,12 +23,12 @@ DROP TABLE IF EXISTS `build_group`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `build_group` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` varchar(30) NOT NULL,
   `title` text NOT NULL,
   `is_deleted` enum('N','Y') NOT NULL DEFAULT 'N',
   `is_active` enum('Y','N') NOT NULL DEFAULT 'Y',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,7 +37,7 @@ CREATE TABLE `build_group` (
 
 LOCK TABLES `build_group` WRITE;
 /*!40000 ALTER TABLE `build_group` DISABLE KEYS */;
-INSERT INTO `build_group` VALUES (1,'Admin','N','Y'),(2,'Normal User','N','Y'),(3,'Chief Executive Officer','N','N'),(4,'Manager','N','Y'),(5,'Superviser','N','Y'),(6,'Chief Associate','N','N'),(7,'Chief Technical Officer','N','N'),(8,'Head Master','N','N');
+INSERT INTO `build_group` VALUES ('GRP00000001','Admin','N','Y'),('GRP00000003','Normal User','N','Y'),('GRP00000005','Chief Executive Officer','N','N'),('GRP00000007','Manager','N','Y'),('GRP00000009','Superviser','N','Y'),('GRP00000011','Chief Associate','N','N'),('GRP00000013','Chief Technical Officer','N','N'),('GRP00000015','Head Master','N','N'),('GRP00000017','New User','N','N'),('GRP00000019','User again','N','N');
 /*!40000 ALTER TABLE `build_group` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -75,8 +75,9 @@ DROP TABLE IF EXISTS `build_sequences`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `build_sequences` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`)
+  `sequence_name` varchar(100) NOT NULL,
+  `value` int(11) DEFAULT NULL,
+  `code` varchar(30) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -86,6 +87,7 @@ CREATE TABLE `build_sequences` (
 
 LOCK TABLES `build_sequences` WRITE;
 /*!40000 ALTER TABLE `build_sequences` DISABLE KEYS */;
+INSERT INTO `build_sequences` VALUES ('GRP_SEQ',21,'GRP'),('USER_SEQ',9,'USR'),('MENU_SEQ',1,'ROLE');
 /*!40000 ALTER TABLE `build_sequences` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -97,14 +99,14 @@ DROP TABLE IF EXISTS `build_user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `build_user` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` varchar(30) NOT NULL,
   `title` text NOT NULL,
   `username` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `is_deleted` enum('N','Y') NOT NULL DEFAULT 'N',
   `is_active` enum('Y','N') NOT NULL DEFAULT 'Y',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -113,13 +115,121 @@ CREATE TABLE `build_user` (
 
 LOCK TABLES `build_user` WRITE;
 /*!40000 ALTER TABLE `build_user` DISABLE KEYS */;
-INSERT INTO `build_user` VALUES (1,'Anup Sharma','anupshrm','anupshrm','N','Y'),(2,'Vikrant Rai','vik','vik','N','Y'),(3,'Soura  Bhattacharya','soura','soura','N','Y'),(4,'Chayan Chatterjee','chayan','chayan','N','Y');
+INSERT INTO `build_user` VALUES ('USR00000001','Anup Sharma','anupshrm','anupshrm','N','Y'),('USR00000003','Vikrant Rai','vik','vik','N','Y'),('USR00000005','Soura  Bhattacharya','soura','soura','N','Y'),('USR00000007','Chayan Chatterjee','chayan','chayan','N','Y');
 /*!40000 ALTER TABLE `build_user` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `build_user_group`
+--
+
+DROP TABLE IF EXISTS `build_user_group`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `build_user_group` (
+  `group_id` varchar(30) NOT NULL,
+  `user_id` varchar(30) NOT NULL,
+  KEY `group_fk1` (`group_id`),
+  KEY `user_fk1` (`user_id`),
+  CONSTRAINT `user_fk1` FOREIGN KEY (`user_id`) REFERENCES `build_user` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `group_fk1` FOREIGN KEY (`group_id`) REFERENCES `build_group` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `build_user_group`
+--
+
+LOCK TABLES `build_user_group` WRITE;
+/*!40000 ALTER TABLE `build_user_group` DISABLE KEYS */;
+INSERT INTO `build_user_group` VALUES ('GRP00000001','USR00000001'),('GRP00000003','USR00000001'),('GRP00000003','USR00000003'),('GRP00000003','USR00000005'),('GRP00000003','USR00000007');
+/*!40000 ALTER TABLE `build_user_group` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Temporary table structure for view `vusers_roles`
+--
+
+DROP TABLE IF EXISTS `vusers_roles`;
+/*!50001 DROP VIEW IF EXISTS `vusers_roles`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE TABLE `vusers_roles` (
+  `group_id` tinyint NOT NULL,
+  `group_title` tinyint NOT NULL,
+  `user_id` tinyint NOT NULL,
+  `user_title` tinyint NOT NULL,
+  `username` tinyint NOT NULL,
+  `password` tinyint NOT NULL
+) ENGINE=MyISAM */;
+SET character_set_client = @saved_cs_client;
+
+--
 -- Dumping routines for database 'building'
 --
+/*!50003 DROP FUNCTION IF EXISTS `GET_ID` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` FUNCTION `GET_ID`(v_CODE VARCHAR(30)) RETURNS varchar(30) CHARSET utf8
+BEGIN	
+  DECLARE v_VALUE INTEGER;
+  SELECT VALUE INTO v_VALUE FROM build_sequences WHERE CODE = v_CODE;
+  UPDATE build_sequences SET value=v_VALUE+2 WHERE CODE = v_CODE;
+	RETURN CONCAT(v_CODE,LPAD(v_VALUE,8,'0'));
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP FUNCTION IF EXISTS `GET_SEQUENCE` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` FUNCTION `GET_SEQUENCE`(SEQ_NAME VARCHAR(100)) RETURNS int(11)
+BEGIN
+	DECLARE v_VALUE INTEGER;
+  SELECT VALUE INTO v_VALUE FROM build_sequences WHERE SEQUENCE_NAME = SEQ_NAME;
+  UPDATE build_sequences SET value=v_VALUE+2 WHERE SEQUENCE_NAME = SEQ_NAME;
+	RETURN v_VALUE;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Final view structure for view `vusers_roles`
+--
+
+/*!50001 DROP TABLE IF EXISTS `vusers_roles`*/;
+/*!50001 DROP VIEW IF EXISTS `vusers_roles`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8 */;
+/*!50001 SET character_set_results     = utf8 */;
+/*!50001 SET collation_connection      = utf8_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `vusers_roles` AS select `bg`.`id` AS `group_id`,`bg`.`title` AS `group_title`,`bu`.`id` AS `user_id`,`bu`.`title` AS `user_title`,`bu`.`username` AS `username`,`bu`.`password` AS `password` from ((`build_group` `bg` left join `build_user_group` `bug` on((`bg`.`id` = `bug`.`group_id`))) left join `build_user` `bu` on((`bu`.`id` = `bug`.`user_id`))) where ((`bg`.`is_active` = 'Y') and (`bg`.`is_deleted` = 'N') and (`bu`.`is_active` = 'Y') and (`bu`.`is_deleted` = 'N')) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -130,4 +240,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-12-09  7:18:08
+-- Dump completed on 2014-12-09 18:30:10
